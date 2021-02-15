6fd5c9811a33fca5ce87fd57933849249864a1d3
Julian Maurel <jmaurel@jahia.com>
2021-02-15 12:13:36 +0100

    PAAS-1444: simplify code

--- src/main/java/org/jahia/modules/healthcheck/servlet/HealthcheckJSONProducer.java
+++ src/main/java/org/jahia/modules/healthcheck/servlet/HealthcheckJSONProducer.java
@@ -96,7 +96,7 @@ public class HealthcheckJSONProducer extends HttpServlet {
                         JSONObject healthcheckerJSON = new JSONObject();
                         String probeSeverity = healthcheckConfig.getProperty(String.format(HealthcheckConstants.PROP_HEALTHCHECK_PROBE_SEVERITY_PARAMETER, probes.get(i).getName()));
                         if (probeSeverity == null) {
-                            probeSeverity = DEFAULT_PROBES_SEVERITY.containsKey(probes.get(i).getName()) ? DEFAULT_PROBES_SEVERITY.get(probes.get(i).getName()) : HealthcheckConstants.PROBE_SEVERITY_LOW_LABEL;
+                            DEFAULT_PROBES_SEVERITY.getOrDefault(probes.get(i).getName(), HealthcheckConstants.PROBE_SEVERITY_LOW_LABEL);
                         }
                         probeSeverity = probeSeverity.toUpperCase();
                         int probeSeverityInt = PROBE_SEVERITY_LEVELS.get(probeSeverity);
